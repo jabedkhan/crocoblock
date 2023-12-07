823f81b7ee4aa7ad26f6ee0224662046c74f6fbe
Md Jabed khan <mdjabedkhan1106@gmail.com>
2023-12-07 16:52:35 +0600

    Add missing file

---
+++ function.php
@@ -0,0 +1,18 @@
+<?php
+
+/*Repeater multiple post field macros with link and title */
+
+add_filter( 'jet-engine/listings/filters-list', function($cb){
+$cb['post_link_by_ids'] = array(
+'cb' => 'jet_get_pretty_post_link',
+'args' => false,
+);
+
+return $cb;
+});
+
+// You need to use this macro: %post-field|post_link_by_ids%
+
+
+
+?>
