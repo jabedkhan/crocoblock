a938147286ea4e83e42807549e3e526c6a8552e5
Md Jabed khan <mdjabedkhan1106@gmail.com>
2024-01-18 02:22:31 +0600

    Single and multiple booking functionality for custom post type

--- jet-booking-add-cpt.php
+++ jet-booking-add-cpt.php
@@ -1,4 +1,5 @@
 <?php
+// ------------- For Single Post Type -------------- 
 // Replace additional-props with your CPT slug
 DEFINE( 'BOOKING_ADDITIONAL_CPT', 'additional-props' );
 
@@ -17,5 +18,31 @@ add_filter( 'jet-booking/settings/get/apartment_post_type', function ( $post_typ
 
 add_filter( 'jet-booking/tools/post-type-args', function( $args ) {
 	$args['post_type'] = [ $args['post_type'], BOOKING_ADDITIONAL_CPT ];
+	return $args;
+} );
+
+
+
+
+
+// ------------- Fpr Multiple Post Type -------------------
+// Replace additional-props with your CPT slugs
+DEFINE( 'BOOKING_ADDITIONAL_CPTS', [ 'additional-props', 'additional-props'] );
+
+add_filter( 'jet-booking/settings/get/apartment_post_type', function ( $post_type ) {
+	if ( is_admin() ) {
+		if ( ! empty( $_GET['post_type'] ) && in_array( $_GET['post_type'], BOOKING_ADDITIONAL_CPTS ) ) {
+			$post_type = $_GET['post_type'];
+		}
+		
+		if ( ! empty( $_GET['post'] ) && in_array( get_post_type( $_GET['post'] ), BOOKING_ADDITIONAL_CPTS ) ) {
+			$post_type = get_post_type( $_GET['post'] );
+		}
+	}
+	return $post_type;
+} );
+
+add_filter( 'jet-booking/tools/post-type-args', function( $args ) {
+	$args['post_type'] = BOOKING_ADDITIONAL_CPTS;
 	return $args;
 } );
